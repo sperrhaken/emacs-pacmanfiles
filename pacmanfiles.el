646028df17c4b90f427a633c3f42c2f6ab8d6816
Felix Dietrich <felix.dietrich@sperrhaken.name>
2014-04-20 13:27:19 +0200

    Change output format and break `pacmanfiles-ediff'
    pacmanfiles-ediff uses the button-label to get the filepath, but the
    buttons are all labled "<--ediff-->" now.

--- pacmanfiles.el
+++ pacmanfiles.el
@@ -10,6 +10,19 @@
 	(delete-region (point-min) (point-max))
 	(let ((pac-files (split-string (shell-command-to-string "locate -e --regexp '\\.pac\\(new\\|save\\|orig\\)$'") "\n"))
 		  (var))
-	  (dolist (var pac-files)
-		(insert-button var 'action 'pacmanfiles-ediff)
-		(insert "\n")))))
+	  (dolist (file-b pac-files)
+		(let* ((mtime (lambda (path) (nth 4 (file-attributes path))))
+			   (file-a (file-name-sans-extension file-b))
+			   (file-a-mtime (funcall mtime file-a))
+			   (file-b-mtime (funcall mtime file-b)))
+		  (insert file-a
+				  "\t"
+				  (format-time-string "%d. %b %Y" file-a-mtime)
+				  "\t")
+		  (insert-button "<--ediff-->" 'action 'pacmanfiles-ediff)
+		  (insert "\t"
+				  (format-time-string "%d. %b %Y" file-b-mtime)
+				  "\t"
+				  file-b
+				  "\n"))))
+	(align-regexp (point-min) (point-max) "\\(\t\\)" 1 1 :repeat)))
