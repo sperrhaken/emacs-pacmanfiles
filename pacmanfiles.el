24cf5331c55161fd274d70f207adae71e9a2a799
Felix Dietrich <felix.dietrich@sperrhaken.name>
2014-04-21 21:53:37 +0200

    Use text-properties to mark the filepaths

--- pacmanfiles.el
+++ pacmanfiles.el
@@ -1,6 +1,24 @@
+(defun pacmanfiles-current-line-file-a ()
+  (save-excursion
+	(let (point-beginning point-end)
+	  (setq point-beginning (line-beginning-position))
+	  (setq point-end (next-single-property-change point-beginning
+												   'pacmanfiles-file-a))
+	  (buffer-substring-no-properties (line-beginning-position) point-end))))
+
+
+(defun pacmanfiles-current-line-file-b ()
+  (save-excursion
+	(let (point-beginning point-end)
+	  (setq point-end (line-end-position))
+	  (setq point-beginning (previous-single-property-change point-end
+															 'pacmanfiles-file-b))
+	  (buffer-substring-no-properties point-beginning (line-end-position)))))
+
+
 (defun pacmanfiles-ediff (button)
-  (let* ((file-b (button-label button))
-		 (file-a (concat "/sudo::" (replace-regexp-in-string "\\.pac\\(new\\|save\\|orig\\)$" "" file-b))))
+  (let ((file-a (pacmanfiles-current-line-file-a))
+		(file-b (pacmanfiles-current-line-file-b)))
 	(ediff-files file-a file-b)))
 
 
@@ -15,6 +33,8 @@
 			   (file-a (file-name-sans-extension file-b))
 			   (file-a-mtime (funcall mtime file-a))
 			   (file-b-mtime (funcall mtime file-b)))
+		  (add-text-properties 0 (length file-a) '(pacmanfiles-file-a t rear-nonsticky t) file-a)
+		  (put-text-property 0 (length file-b) 'pacmanfiles-file-b t file-b)
 		  (insert file-a
 				  "\t"
 				  (format-time-string "%d. %b %Y" file-a-mtime)
